1793146232c81df5e634289ba1486d293ff1d12b
thisisyomans <tanejamm@gmail.com>
2018-08-26 14:51:12 -0700

    os transfer

--- game.py
+++ game.py
@@ -31,7 +31,7 @@ gameover = pygame.image.load("resources/images/gameover.png")
 youwin = pygame.image.load("resources/images/youwin.png")
 
 while 1:
-    exitcode = 0
+    exitcode = 1
     badtimer -= 1
 
     screen.fill(0)
@@ -141,39 +141,35 @@ while 1:
             if event.key == K_d:
                 keys[3] = False
 
-    #if keys[0]:
-    #    playerpos[1] -= 5
-    #elif keys[2]:
-    #    playerpos[1] += 5
-    #if keys[1]:
-    #    playerpos[0] -= 5
-    #elif keys[3]:
-    #    playerpos[0] += 5
+    if keys[0]:
+        playerpos[1] -= 5
+    elif keys[2]:
+        playerpos[1] += 5
+    if keys[1]:
+        playerpos[0] -= 5
+    elif keys[3]:
+        playerpos[0] += 5
 
-    #if pygame.time.get_ticks() >= 90000:
-    #    running = 0
-    #    exitcode = 1
-    #if healthvalue <= 0:
-    #    running = 0
-    #    exitcode = 0
-    #if acc[1] != 0:
-    #    accuracy = acc[0] * 1.0 / acc[1] * 100
-    #else:
-    #    accuracy = 0
+    if healthvalue <= 0:
+        exitcode = 0
+    if acc[1] != 0:
+        accuracy = acc[0] * 1.0 / acc[1] * 100
+    else:
+        accuracy = 0
 
-    #if exitcode == 0:
-    #    pygame.font.init()
-    #    font = pygame.font.Font(None, 24)
-    #    text = font.render("Accuracy: " + str(accuracy) + "%", True, (255, 0, 0))
-    #    textRect = text.get_rect()
-    #    textRect.centerx = screen.get_rect().centerx
-    #    textRect.centery = screen.get_rect().centery + 24
-    #    screen.blit(gameover, (0, 0))
-    #    screen.blit(text, textRect)
+    if exitcode == 0:
+        pygame.font.init()
+        font = pygame.font.Font(None, 24)
+        text = font.render("Accuracy: " + str(accuracy) + "%", True, (255, 0, 0))
+        textRect = text.get_rect()
+        textRect.centerx = screen.get_rect().centerx
+        textRect.centery = screen.get_rect().centery + 24
+        screen.blit(gameover, (0, 0))
+        screen.blit(text, textRect)
 
-    #while 1:
-    #    for event in pygame.event.get():
-    #        if event.type == pygame.QUIT:
-    #            pygame.quit()
-    #            exit(0)
-    #    pygame.display.flip()
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit(0)
+        pygame.display.flip()
